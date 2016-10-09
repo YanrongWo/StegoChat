a0a88302fba7dd257ec1d4450117bda114b0fcc4
Rona (Yanrong Wo) <yw2513@columbia.edu>
2016-10-08 21:39:57 -0400

    Added eventsource

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,3 +1,9 @@
 $( document ).ready(function() {
 	$('#myModal').modal('show')
+    var evtSrc = new EventSource("/subscribe");
+    evtSrc.onmessage = function(e) {
+    	console.log(e.data);
+    	$("#convo_section").append('<div>'+e.data+'</div>')
+    };
 }
+
